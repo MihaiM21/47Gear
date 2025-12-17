9b7f60b97057c0a97d6aadc2007a348aeb46e40b
Mihai <mihai.marinescu2112@yahoo.com>
2025-12-17 15:14:05 +0200

    added float whatsapp button on all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Navbar } from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
 import { CartProvider } from "@/components/cart/cart-context";
+import WhatsAppFloat from "@/components/whatsapp-float";
 import { cookies } from "next/headers";
 import { getCart } from "@/lib/shopify";
 
@@ -29,6 +30,7 @@ export default async function RootLayout({
           <Navbar />
           {children}
           <Footer />
+          <WhatsAppFloat />
         </CartProvider>
       </body>
     </html>
